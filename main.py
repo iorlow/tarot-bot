5d1a6711d5c2e5e38663c406cc8e0d3c2d368dfe
iorlow <igor_orlow@hotmail.com>
2026-02-17 21:56:34 -0300

    Modify webhook to include button options
    Updated the welcome message and added a button list for user interaction.

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ ZAPI_INSTANCE_ID = os.getenv("ZAPI_INSTANCE_ID")
 ZAPI_TOKEN = os.getenv("ZAPI_TOKEN")
 ZAPI_CLIENT_TOKEN = os.getenv("ZAPI_CLIENT_TOKEN")
 
-ZAPI_URL = f"https://api.z-api.io/instances/{ZAPI_INSTANCE_ID}/token/{ZAPI_TOKEN}/send-text"
+#ZAPI_URL_TEXT = f"https://api.z-api.io/instances/{ZAPI_INSTANCE_ID}/token/{ZAPI_TOKEN}/send-text"
+ZAPI_URL_BUTTON = f"https://api.z-api.io/instances/{ZAPI_INSTANCE_ID}/token/{ZAPI_TOKEN}/send-button-list"
 
 @app.post("/webhook")
 async def webhook(request: Request):
@@ -28,10 +29,25 @@ async def webhook(request: Request):
     }
     payload = {
         "phone": phone,
-        "message": "✨ Seja bem-vindo(a). O universo já está ouvindo você..."
+        "message": "✨ Desejamos as boas vindas! O universo já está ouvindo você... Escolha o que você quer fazer a seguir:",
+        "buttonList": {
+            "buttons": [
+              {
+                "id": "1",
+                "label": "Leitura de cartas do tarot"
+              },
+              {
+                "id": "2",
+                "label": "Palpite de números da loteria"
+              }
+                    ]
+  }
+    
+    
     }
 
-    response = requests.post(ZAPI_URL, json=payload, headers=headers)
+    response = requests.post(ZAPI_URL_BUTTON, json=payload, headers=headers)
+    
     print("Status envio:", response.status_code)
     print("Responta Z-API:", response.text)
     
