883a7950153f3325dbd7839f0d2f25458ffef593
iorlow <igor_orlow@hotmail.com>
2026-02-23 23:44:43 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,41 +3,48 @@ from fastapi import FastAPI, Request
 from database import engine
 from models import Base
 from services import obter_ou_criar_usuario, atualizar_etapa
+import variaveis
 
 app = FastAPI()
 
 # Cria tabelas se não existirem
 Base.metadata.create_all(engine)
 
-
 @app.post("/webhook")
 async def webhook(request: Request):
-    body = await request.json()
-
-    # Ajuste conforme payload real da Z-API
-    telefone = body.get("phone")
-    mensagem = body.get("message")
-
-    if not telefone:
-        return {"erro": "Telefone não informado"}
-
-    usuario = obter_ou_criar_usuario(telefone)
-
-    # Exemplo simples de fluxo
-    if usuario.etapa_fluxo == "inicio":
-        
-        nova_etapa = "aguardando_descricao"
-    elif usuario.etapa_fluxo == "aguardando_descricao":
-        nova_etapa = "finalizado"
+    if data.get("fromMe"):
+        print("Mensagem ignorada (enviada pelo próprio BOT)")
+        return {"status": "ignored"}
     else:
-        nova_etapa = "inicio"
-
-        
-    usuario = atualizar_etapa(usuario.id, nova_etapa)
-    print("Telefone do usuário: ", usuario.telefone)
-
-    return {
-        "status": "ok",
-        "usuario_id": usuario.id,
-        "etapa_atual": usuario.etapa_fluxo
-    }
+        body = await request.json()
+        # Ajuste conforme payload real da Z-API
+        telefone = body.get("phone")
+        mensagem = body.get("message")
+    
+        if not telefone:
+            return {"erro": "Telefone não informado"}
+    
+        usuario = obter_ou_criar_usuario(telefone)
+    
+        # Exemplo simples de fluxo
+        if usuario.etapa_fluxo == "inicio":
+            
+            nova_etapa = "aguardando_descricao"
+            
+        elif usuario.etapa_fluxo == "aguardando_descricao":
+            
+            nova_etapa = "finalizado"
+            
+        else:
+            
+            nova_etapa = "inicio"
+    
+            
+        usuario = atualizar_etapa(usuario.id, nova_etapa)
+        print("Telefone do usuário: ", usuario.telefone)
+    
+        return {
+            "status": "ok",
+            "usuario_id": usuario.id,
+            "etapa_atual": usuario.etapa_fluxo
+        }
