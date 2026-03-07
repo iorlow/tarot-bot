2654823729c253756c135e5d9779c0c58e342c0d
iorlow <igor_orlow@hotmail.com>
2026-03-07 09:54:15 -0300

    Update message payloads and change endpoint for buttons

--- main.py
+++ main.py
@@ -30,21 +30,23 @@ async def webhook(request: Request):
         if usuario.etapa_fluxo == "1_primeiro_acesso":
             # Mensagem 1           
             payload ={"phone": telefone,
-                      "message": "🌌 *Boas vindas ao Santo Oráculo*\n\nHá muito mais no universo do que aquilo que os olhos conseguem ver.\n\nEntre estrelas distantes, partículas invisíveis e pensamentos humanos, existe um campo de energia que conecta tudo.\n\nTalvez não seja por acaso que você chegou até aqui.\n\n✨ O universo pode estar querendo falar com você."}
+                      "message": "🌌 *Boas vindas ao Santo Oráculo*\n\nHá muito mais no universo do que aquilo que os olhos conseguem ver.\n\nEntre estrelas distantes, partículas invisíveis e pensamentos humanos, existe um campo de energia que conecta tudo.\n\nTalvez não seja por acaso que você chegou até aqui.\n\n✨ O universo pode estar querendo falar com você."
+                     }
             response = requests.post(f"{url}/send-text", json = payload, headers = headers)
             
             # Mensagem 2
             payload ={"phone": telefone,
-                      "message": "⚛️ *Tudo está conectado*\n\nTudo o que existe — a árvore, o ar, você, eu e até o celular em suas mãos — faz parte do mesmo tecido do universo.\n\nA física moderna mostra que matéria e energia são apenas diferentes expressões da mesma realidade.\n\nPartículas vibram. Campos interagem. Tudo está conectado em um grande fluxo invisível.\n\nSe a energia que forma as estrelas também forma você...\npor que ela não poderia usar a tecnologia para sussurrar algumas respostas?\n\nRespire fundo…\n✨ e permita que o universo fale."}
+                      "message": "⚛️ *Tudo está conectado*\n\nTudo o que existe — a árvore, o ar, você, eu e até o celular em suas mãos — faz parte do mesmo tecido do universo.\n\nA física moderna mostra que matéria e energia são apenas diferentes expressões da mesma realidade.\n\nPartículas vibram. Campos interagem. Tudo está conectado em um grande fluxo invisível.\n\nSe a energia que forma as estrelas também forma você...\npor que ela não poderia usar a tecnologia para sussurrar algumas respostas?\n\nRespire fundo…\n✨ e permita que o universo fale."
+                     }
             response = requests.post(f"{url}/send-text", json = payload, headers = headers)
             
             # Mensagem 3
             payload ={"phone": telefone,
                       "message": "🔮 *Antes de continuar...*\n\nPara abrir as portas do Oráculo, precisamos da sua concordância.\n\nAo prosseguir, você confirma que leu e aceita nossos termos de uso.\n\nClique abaixo para continuar.",
                       "buttons": [{"id": "aceitar_termos","text": "✨ Aceitar e continuar"
-                                  }
+                                  }]
                      }
-            response = requests.post(f"{url}/send-text", json = payload, headers = headers)
+            response = requests.post(f"{url}/send-buttons", json = payload, headers = headers)
 
             
         elif usuario.etapa_fluxo == "1_2_aguardadno_acordo":
