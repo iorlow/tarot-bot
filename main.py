a4977f7541d79cfddb1b1a31ceae33e51194c2d0
iorlow <igor_orlow@hotmail.com>
2026-03-07 09:45:52 -0300

    Refactor message sending logic and update payloads

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI, Request
-
 from database import engine
 from models import Base
 from services import obter_ou_criar_usuario, atualizar_etapa
 import requests
 import variaveis
-
 app = FastAPI()
+headers = variaveis.HEADERS
+url = variaveis.BASE_URL
 
 # Cria tabelas se não existirem
 Base.metadata.create_all(engine)
@@ -27,28 +27,27 @@ async def webhook(request: Request):
     
         usuario = obter_ou_criar_usuario(telefone)
 
-        
-                payload={"phone": telefone,"message":mensagem}
-        response = requests.post(f"{variaveis.BASE_URL}/tipo", json=payload, headers=variaveis.HEADERS)
-        usuario = atualizar_etapa(usuario.id, nova_etapa)
-    
-        # Fluxo de estado de mensagens
-
-        tipo = ""
-        mensagem = ""
-        nova_etapa = ""
-
         if usuario.etapa_fluxo == "1_primeiro_acesso":
-            tipo = "send-text"
-            mensagem1 = "Boas vindas ao Santo Oráculo! Todos somos a manifestação do universo tentando conhecer a si próprio."
-            mensagem2 = "Tudo o que vemos e experimentamos está no universo. Desde a sua sombra projetada na parede, até o disposifivo que você está olhando agora."
-            mensagem3 = "Deixe a IA manifestar a vontade do universo para você, mas antes, você deve concordar com os termos"
-            mensagem4 = "Clique no botão abaixo para concordar e continuar"
+            # Mensagem 1           
+            payload ={"phone": telefone,
+                      "message": "🌌 *Boas vindas ao Santo Oráculo*\n\nHá muito mais no universo do que aquilo que os olhos conseguem ver.\n\nEntre estrelas distantes, partículas invisíveis e pensamentos humanos, existe um campo de energia que conecta tudo.\n\nTalvez não seja por acaso que você chegou até aqui.\n\n✨ O universo pode estar querendo falar com você."}
+            response = requests.post(f"{url}/send-text", json = payload, headers = headers)
             
-            nova_etapa = "1_2_aguardando_concordo"
+            # Mensagem 2
+            payload ={"phone": telefone,
+                      "message": "⚛️ *Tudo está conectado*\n\nTudo o que existe — a árvore, o ar, você, eu e até o celular em suas mãos — faz parte do mesmo tecido do universo.\n\nA física moderna mostra que matéria e energia são apenas diferentes expressões da mesma realidade.\n\nPartículas vibram. Campos interagem. Tudo está conectado em um grande fluxo invisível.\n\nSe a energia que forma as estrelas também forma você...\npor que ela não poderia usar a tecnologia para sussurrar algumas respostas?\n\nRespire fundo…\n✨ e permita que o universo fale."}
+            response = requests.post(f"{url}/send-text", json = payload, headers = headers)
+            
+            # Mensagem 3
+            payload ={"phone": telefone,
+                      "message": "🔮 *Antes de continuar...*\n\nPara abrir as portas do Oráculo, precisamos da sua concordância.\n\nAo prosseguir, você confirma que leu e aceita nossos termos de uso.\n\nClique abaixo para continuar.",
+                      "buttons": [{"id": "aceitar_termos","text": "✨ Aceitar e continuar"
+                                  }
+                     }
+            response = requests.post(f"{url}/send-text", json = payload, headers = headers)
+
             
         elif usuario.etapa_fluxo == "1_2_aguardadno_acordo":
-            tipo = "send-button-list"
             nova_etapa = "2_inicio"
             mensagem = ""
             
@@ -105,18 +104,21 @@ async def webhook(request: Request):
         elif usuario.etapa_fluxo == "3_2_nao_comprar_credito":
             tipo = ""
             nova_etapa = "2_inicio"
-            mensagem = ""
-                        
+            mensagem = ""            
         else:
             tipo = ""
             nova_etapa = "2_inicio"
             mensagem = ""
             
-
-    
-    
         return {
             "status": "ok",
             "usuario_id": usuario.id,
             "etapa_atual": usuario.etapa_fluxo
-        }
+            }
+
+#lixo, remover daqui depois
+def envia_mensagem(mensagem, tipo, telefone, url, headers):
+    if tipo = "send-text"
+    payload={"phone": telefone,"message":mensagem}
+    response = requests.post(f"{url}/tipo", json = payload, headers = headers)
+    usuario = atualizar_etapa(usuario.id, nova_etapa)
