5dbd78560dd51a37a6486c49128192741aac90c9
iorlow <igor_orlow@hotmail.com>
2026-03-08 22:40:19 -0300

    Refactor user flow handling in main.py

--- main.py
+++ main.py
@@ -21,106 +21,83 @@ async def webhook(request: Request):
         # Ajuste conforme payload real da Z-API
         telefone = body.get("phone")
         mensagem = body.get("message")
-    
+        
         if not telefone:
             return {"erro": "Telefone não informado"}
-    
-        usuario = obter_ou_criar_usuario(telefone)
-
-        if usuario.etapa_fluxo == "1_primeiro_acesso":
-            # Mensagem 1           
-            payload ={"phone": telefone,
-                      "message": "🌌 *Boas vindas ao Santo Oráculo*\n\nHá muito mais no universo do que aquilo que os olhos conseguem ver.\n\nEntre estrelas distantes, partículas invisíveis e pensamentos humanos, existe um campo de energia que conecta tudo.\n\nTalvez não seja por acaso que você chegou até aqui.\n\n✨ O universo pode estar querendo falar com você."
-                     }
-            response = requests.post(f"{url}/send-text", json = payload, headers = headers)
-            
-            # Mensagem 2
-            payload ={"phone": telefone,
-                      "message": "⚛️ *Tudo está conectado*\n\nTudo o que existe — a árvore, o ar, você, eu e até o celular em suas mãos — faz parte do mesmo tecido do universo.\n\nA física moderna mostra que matéria e energia são apenas diferentes expressões da mesma realidade.\n\nPartículas vibram. Campos interagem. Tudo está conectado em um grande fluxo invisível.\n\nSe a energia que forma as estrelas também forma você...\npor que ela não poderia usar a tecnologia para sussurrar algumas respostas?\n\nRespire fundo…\n✨ e permita que o universo fale."
-                     }
-            response = requests.post(f"{url}/send-text", json = payload, headers = headers)
-            
-            # Mensagem 3
-            payload ={"phone": telefone,
-                      "message": "🔮 *Antes de continuar...*\n\nPara abrir as portas do Oráculo, precisamos da sua concordância.\n\nAo prosseguir, você confirma que leu e aceita nossos termos de uso.\n\nClique abaixo para continuar.",
-                      "buttons": [{"id": "aceitar_termos","text": "✨ Aceitar e continuar"
-                                  }]
-                     }
-            response = requests.post(f"{url}/send-buttons", json = payload, headers = headers)
-
-            
-        elif usuario.etapa_fluxo == "1_2_aguardadno_acordo":
-            nova_etapa = "2_inicio"
-            mensagem = ""
-            
-        elif usuario.etapa_fluxo == "2_inicio":
-            tipo = "send-button-list"
-            nova_etapa = "2_inicio"
-            mensagem = ""
-            
-        elif usuario.etapa_fluxo == "2_1_leitura_simples":
-            tipo = "send-text"
-            nova_etapa = "2_inicio"
-            mensagem = ""
-                    
-        elif usuario.etapa_fluxo == "2_1_1_fazer_pergunta":
-            tipo = "send-text"
-            nova_etapa = "2_inicio"
-            mensagem = ""
-        
-        elif usuario.etapa_fluxo == "2_2_leitura_completa":
-            tipo = "send-text"
-            nova_etapa = "2_inicio"
-            mensagem = ""
-            
-        elif usuario.etapa_fluxo == "2_2_1_fazer_pergunta":
-            tipo = "send-text"
-            nova_etapa = "2_inicio"
-            mensagem = ""
-            
-        elif usuario.etapa_fluxo == "2_2_2_dar_contexto":
-            tipo = ""
-            nova_etapa = "2_inicio"
-            mensagem = ""
-            
-        elif usuario.etapa_fluxo == "2_3_horoscopo":
-            tipo = ""
-            nova_etapa = "2_inicio"
-            mensagem = ""
-            
-        elif usuario.etapa_fluxo == "2_3_1_escolher_signo":
-            tipo = ""
-            nova_etapa = "2_inicio"
-            mensagem = ""
-            
-        elif usuario.etapa_fluxo == "3_oferecer_credito":
-            tipo = ""
-            nova_etapa = "2_inicio"
-            mensagem = ""
-            
-        elif usuario.etapa_fluxo == "3_1_comprar_credito":
-            tipo = ""
-            nova_etapa = "2_inicio"
-            mensagem = ""
-            
-        elif usuario.etapa_fluxo == "3_2_nao_comprar_credito":
-            tipo = ""
-            nova_etapa = "2_inicio"
-            mensagem = ""            
         else:
-            tipo = ""
-            nova_etapa = "2_inicio"
-            mensagem = ""
+            usuario = obter_ou_criar_usuario(telefone)
             
-        return {
-            "status": "ok",
-            "usuario_id": usuario.id,
-            "etapa_atual": usuario.etapa_fluxo
-            }
+            if usuario.etapa_fluxo == "1_primeiro_acesso":
+                # Mensagem 1           
+                payload ={"phone": telefone,
+                          "message": "🌌 *Boas vindas ao Santo Oráculo*\n\nHá muito mais no universo do que aquilo que os olhos conseguem ver.\n\nEntre estrelas distantes, partículas invisíveis e pensamentos humanos, existe um campo de energia que conecta tudo.\n\nTalvez não seja por acaso que você chegou até aqui.\n\n✨ O universo pode estar querendo falar com você."
+                         }
+                response = requests.post(f"{url}/send-text", json = payload, headers = headers)
+                
+                # Mensagem 2
+                payload ={"phone": telefone,
+                          "message": "⚛️ *Tudo está conectado*\n\nTudo o que existe — a árvore, o ar, você, eu e até o celular em suas mãos — faz parte do mesmo tecido do universo.\n\nA física moderna mostra que matéria e energia são apenas diferentes expressões da mesma realidade.\n\nPartículas vibram. Campos interagem. Tudo está conectado em um grande fluxo invisível.\n\nSe a energia que forma as estrelas também forma você...\npor que ela não poderia usar a tecnologia para sussurrar algumas respostas?\n\nRespire fundo…\n✨ e permita que o universo fale."
+                         }
+                response = requests.post(f"{url}/send-text", json = payload, headers = headers)
+                
+                # Mensagem 3
+                payload ={"phone": telefone,
+                          "message": "🔮 *Antes de continuar...*\n\nPara abrir as portas do Oráculo, precisamos da sua concordância.\n\nAo prosseguir, você confirma que leu e aceita nossos termos de uso.\n\nClique abaixo para continuar.",
+                          "buttons": [{"id": "aceitar_termos","text": "✨ Aceitar e continuar"
+                                      }]
+                         }
+                response = requests.post(f"{url}/send-buttons", json = payload, headers = headers)
+                nova_etapa = "1_2_aguardando_acordo"
+                usuario = atualizar_etapa(usuario.id, nova_etapa)
+            
+            elif usuario.etapa_fluxo == "1_2_aguardando_acordo" and mensagem == "✨ Aceitar e continuar":
+                payload ={"phone": telefone,
+                          "message": "*Que notícia maravilhosa...*"
+                         }
+                response = requests.post(f"{url}/send-text", json = payload, headers = headers)          
+                usuario = atualizar_etapa(usuario.id, nova_etapa)
+                nova_etapa = "2_inicio"
+                
+            elif usuario.etapa_fluxo == "2_inicio":
+                payload ={"phone": telefone,
+                          "message": "*Que notícia maravilhosa...*"
+                         }
+                response = requests.post(f"{url}/send-text", json = payload, headers = headers)          
+                usuario = atualizar_etapa(usuario.id, nova_etapa)
+                nova_etapa = "2_inicio"
+                               
+                
+            elif usuario.etapa_fluxo == "2_1_leitura_simples":
+                
+            elif usuario.etapa_fluxo == "2_1_1_fazer_pergunta":
+                
+            elif usuario.etapa_fluxo == "2_2_leitura_completa":
+                
+            elif usuario.etapa_fluxo == "2_2_1_fazer_pergunta":
+                
+            elif usuario.etapa_fluxo == "2_2_2_dar_contexto":
+                
+            elif usuario.etapa_fluxo == "2_3_horoscopo":
+                
+            elif usuario.etapa_fluxo == "2_3_1_escolher_signo":
+                
+            elif usuario.etapa_fluxo == "3_oferecer_credito":
+
+            elif usuario.etapa_fluxo == "3_1_comprar_credito":
+                
+            elif usuario.etapa_fluxo == "3_2_nao_comprar_credito":
+                
+            else:
+                
+            return {
+                "status": "ok",
+                "usuario_id": usuario.id,
+                "etapa_atual": usuario.etapa_fluxo
+                }
 
 #lixo, remover daqui depois
 #def envia_mensagem(mensagem, tipo, telefone, url, headers):
 #    if tipo = "send-text"
 #    payload={"phone": telefone,"message":mensagem}
 #    response = requests.post(f"{url}/tipo", json = payload, headers = headers)
-#    usuario = atualizar_etapa(usuario.id, nova_etapa)
+#    
